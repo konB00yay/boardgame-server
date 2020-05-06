521cff44728980eadc44843a9cb4665d09d4392c
Konnor <kbeaulier@wisc.edu>
2020-05-05 17:54:21 -0700

    track player positions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 var PLAYER_COUNTER = {};
+var PLAYER_POSITIONS = {};
 
 io.on("connection", socket => {
   socket.on("rooms", room => {
@@ -10,14 +11,17 @@ io.on("connection", socket => {
     socket.join(lobby);
     if (room.action == "create") {
       PLAYER_COUNTER[lobby] = 1;
+      PLAYER_POSITIONS[lobby] = {};
+      PLAYER_POSITIONS[lobby][1] = 1;
     } else {
       PLAYER_COUNTER[lobby] = PLAYER_COUNTER[lobby] + 1;
-      io.to(lobby).emit("joined", PLAYER_COUNTER[lobby]);
+      PLAYER_POSITIONS[lobby][PLAYER_COUNTER[lobby]] = 1;
+      io.to(lobby).emit("joined", PLAYER_POSITIONS[lobby]);
     }
   });
 
   socket.on("players", room => {
-    io.to(room).emit("players", PLAYER_COUNTER[room]);
+    io.to(room).emit("players", PLAYER_POSITIONS[room]);
   });
 });
 
