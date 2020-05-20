f9c20a74d369340c53a479abc2df8a67202eb78f
Konnor <kbeaulier@wisc.edu>
2020-05-19 21:23:16 -0700

    set timeout

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, { pingTimeout: 600000 });
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
